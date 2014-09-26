8b54606a05fcd23ae6e9367d92ba707575f184f3
Ramiro Gómez <code@ramiro.org>
2014-09-26 17:48:48 +0200

    main functionality implemented, still need a sane solution for comparing URLs in test

--- affurl/affurl.py
+++ affurl/affurl.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
+from urllib import urlencode
+from urlparse import parse_qs, urlparse, urlunparse
+from domain_parser import domain_parser
+
 
 def convert(url, mapping):
     '''Convert given URL into affiliate URL based on mapping.
 
     mapping maps domains with URL paramater/value pairs to add or replace in
-    given URL.
+    given URL. Parameter values must be specified as lists.
 
     Example mapping for various amazon domains:
         {
-            'amazon.com': {'tag': 'affurl-20'},
-            'amazon.de': {'tag': 'affurl-21'},
-            'amazon.co.uk': {'tag': 'afflink-21'}
+            'amazon.com': {'tag': ['affurl-20']},
+            'amazon.de': {'tag': ['affurl-21']},
+            'amazon.co.uk': {'tag': ['afflink-21']}
         }
     '''
 
@@ -18,16 +22,24 @@ def convert(url, mapping):
     if not new_url.netloc:
         return None  # rather raise an Exception?
 
-    # Inser domain check below using domain_extract package
-    # if new_url.netloc != 'www.amazon.com':
-    #     return url
+    # Parse_domain returns a tuple like ('co.uk', 'amazon', 'www')
+    domain = '.'.join(domain_parser.parse_domain(new_url.netloc)[:2][::-1])
+
+    # Leave URLs with no matching domain as they are
+    if domain not in mapping:
+        return url
+
+    params = mapping[domain]
 
     # make sure this is a product URL. FIXME why is this important, which URLs
-    # do not work when tag param is added?
+    # do not work when tag param is added? Maybe because in the previous
+    # version all other params were removed?
+
     # if '/dp/' not in new_url.path and '/gp/product/' not in new_url.path:
     #     return url
 
-    # remove original querystring and add affiliate tag
-    new_url = new_url[:4] + (
-        urlencode({'tag': affiliate_tag}, True), ) + new_url[5:]
+    q = parse_qs(new_url.query)
+    q.update(params)
+    new_url = new_url[:4] + (urlencode(q, True), ) + new_url[5:]
+
     return urlunparse(new_url)
--- tests/test_affurl.py
+++ tests/test_affurl.py
@@ -27,6 +27,8 @@ class TestAffurl(unittest.TestCase):
         # the expected result.
         # FIXME Problem needs to sort URL params for tests to work or another idea!
         urls = [
+            # URL that should stay the same
+            ('http://example.com/path?q=1', 'http://example.com/path?q=1'),
             # home page
             ('http://www.amazon.com/', 'http://www.amazon.com/?tag=affurl-20'),
             # product page
@@ -34,7 +36,8 @@ class TestAffurl(unittest.TestCase):
              'http://www.amazon.com/Python-Language-Reference-Manual/dp/1906966141/ref=la_B0034OPA4K_1_1?s=books&ie=UTF8&qid=1411728826&sr=1-1&tag=affurl-20')
         ]
 
-        for
+        for url in urls:
+            self.assertEqual(url[1], affurl.convert(url[0], self.mapping))
 
     def tearDown(self):
         pass
